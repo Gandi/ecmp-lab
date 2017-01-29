adb04b828bb99af78f958dfdd9100ea92a5d3d57
Nikita Kozlov <nikita@elyzion.net>
2017-01-29 18:17:40 +0100

    add more servers

--- bird.router/init.sh
+++ bird.router/init.sh
@@ -4,5 +4,5 @@ ip -6 addr add $IP6_1 dev eth0
 ip -6 addr add $IP6_2 dev eth1
 
 mkdir -p /run/bird
-/usr/sbin/bird -c /etc/bird.d/bird.conf -s /run/bird/bird.ctl
-/usr/sbin/bird6 -c /etc/bird.d/bird6.conf -s /run/bird/bird6.ctl -d
+/usr/sbin/bird6 -c /etc/bird.d/bird6.conf -s /run/bird/bird6.ctl
+/usr/sbin/bird -c /etc/bird.d/bird.conf -s /run/bird/bird.ctl -d
